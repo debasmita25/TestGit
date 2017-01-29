fe41e38a3080f62effd839688a9a6201cc22ffdf
Debasmita <Debasmita@DESKTOP-HUDNDMF>
2017-01-30 00:25:28 +0530

    new maven project
    Change-Id: Ia75fadbbd8a901bfd3fcf96c19b14b63a05723e9
    Signed-off-by: Debasmita <Debasmita@DESKTOP-HUDNDMF>

---
+++ MavenProjectDemo/src/main/java/com/debasmita25/MavenProjectDemo/App.java
@@ -0,0 +1,13 @@
+package com.debasmita25.MavenProjectDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
